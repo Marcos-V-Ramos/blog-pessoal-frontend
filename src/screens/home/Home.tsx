3938c76bbff18a306878dc474d67fdff89db7f2c
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-05-15 09:43:08 -0300

    Uploading first React Component

---
+++ src/screens/home/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+            <img 
+                src="https://i.imgur.com/H88yIo2.png" 
+                alt="Imagem png da tela principal."
+                width="500"
+                height="500"
+            />
+        </>
+    );
+};
+
+export default Home;
