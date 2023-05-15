1719067074f453579e7cf9ce4226d82ad2678974
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-05-15 10:31:23 -0300

    Utilizando o MaterialUI

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -4,12 +4,11 @@ import './Home.css';
 const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <h1 className='titulo'>Home</h1>
             <img 
                 src="https://i.imgur.com/H88yIo2.png" 
                 alt="Imagem png da tela principal."
-                width="500"
-                height="500"
+                className='img'
             />
         </>
     );
