c8934ba8424ffc173f8ffae5a8195f8158273cbd
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-05-23 17:02:58 -0300

    Implementing cadastroUsuario and model User.ts

--- src/screens/cadastroUsuario/cadastroUsuario.tsx
+++ src/screens/cadastroUsuario/cadastroUsuario.tsx
@@ -1,15 +1,66 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import "./cadastroUsuario.css"
 import { Box, Button, Grid, TextField, Typography } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import User from "../../models/User";
+import { cadastroUsuario as cadUser } from "../../services/Service";
 
-function cadastroUsuario() {
+function CadastroUsuario() {
+
+    let navigate = useNavigate()
+    
+    const[confirmarSenha, setConfirmarSenha] = useState<string>("")
+    const[linkFoto, setLinkFoto] = useState<string>("")
+    
+    const[user, setUser] = useState<User>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        foto: '',
+        senha: ''
+    })
+
+    const [userResult, setUserResult] = useState<User>({
+        id: 0,
+        nome: '',
+        usuario: '',
+        foto: '',
+        senha: ''
+    })
+
+    useEffect(()=>{
+        if (userResult.id !== 0) {
+            navigate('/login')
+            console.log(userResult)
+        }
+    }, [userResult])
+
+    const confirmarSenhaHandle = (e: ChangeEvent<HTMLInputElement>) => {
+        setConfirmarSenha(e.target.value)
+    }
+
+    const updatedModel = (e: ChangeEvent<HTMLInputElement>) => {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const onSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (confirmarSenha === user.senha) {
+            cadUser(`/usuarios/cadastrar`, user, setUserResult)
+            alert('Usuário cadastrado com sucesso.')
+        } else {
+            alert('Dados inconsistentes. Favor arrumar.')
+        }
+    }
 
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center">
             <Grid item xs={6} className="fundoImagem"></Grid>
             <Grid item xs={6}>
-                <form>
+                <form onSubmit={onSubmit}>
                     <Box className="camposForm">
                         <Typography
                             variant="h3"
@@ -31,6 +82,8 @@ function cadastroUsuario() {
                             margin="normal"
                             fullWidth
                             type="text"
+                            value={user.nome}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
                         />
 
                         <TextField
@@ -42,29 +95,53 @@ function cadastroUsuario() {
                             margin="normal"
                             fullWidth
                             type="text"
+                            value={user.usuario}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
                         />
-
                         <TextField
-                            id="senha"
-                            label="Senha"
-                            placeholder="Digite a sua senha:"
+                            id="linkFoto"
+                            label="Insira o Link da sua foto"
+                            placeholder="Insira o nome de usuário:"
                             variant="outlined"
-                            name="senha"
+                            name="foto"
                             margin="normal"
                             fullWidth
-                            type="password"
+                            type="text"
+                            value={user.foto}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
                         />
+                        <div 
+                            className="imagemUsuario"
+                            style={{backgroundImage: user.foto !== '' ? `url(${user.foto})` : 'url(https://i.imgur.com/qDUPJ43_d.webp?maxwidth=760&fidelity=grand)'}}
+                        >
+                        </div>
 
-                        <TextField
-                            id="confirmarSenha"
-                            label="Confirme a senha"
-                            placeholder="Digite novamente a sua senha:"
-                            variant="outlined"
-                            name="confirmarSenha"
-                            margin="normal"
-                            fullWidth
-                            type="password"
-                        />
+                        <Box className="boxSenha">
+                            <TextField
+                                id="senha"
+                                label="Senha"
+                                placeholder="Digite a sua senha:"
+                                variant="outlined"
+                                name="senha"
+                                margin="normal"
+                                fullWidth
+                                type="password"
+                                value={user.senha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                            />
+                            <TextField
+                                id="confirmarSenha"
+                                label="Confirme a senha"
+                                placeholder="Digite novamente a sua senha:"
+                                variant="outlined"
+                                name="confirmarSenha"
+                                margin="normal"
+                                fullWidth
+                                type="password"
+                                value={confirmarSenha}
+                                onChange={(e: ChangeEvent<HTMLInputElement>) => confirmarSenhaHandle(e)}
+                            />
+                        </Box>
 
                         <Box
                             marginTop={2}
@@ -100,4 +177,4 @@ function cadastroUsuario() {
     );
 }
 
-export default cadastroUsuario;
+export default CadastroUsuario; 
